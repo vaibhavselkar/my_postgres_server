d7f49f06b185dcc5226f610b23b85bcc2f32b7bc
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2024-04-02 16:43:43 +0530

    Update index.js - post and get request added for writing section

--- index.js
+++ index.js
@@ -73,6 +73,30 @@ client.connect()
   }
 });
 
+  app.get('/prompts', async (req, res) => {
+    try {
+      const query = 'SELECT * FROM prompts';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'writing_prompts' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'writing_prompts' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+  app.post('/writing_response', async (req, res) => {
+    try {
+      const { username, prompt_id, writing_response } = req.body; // Corrected field name 
+      const result = await client.query('INSERT INTO writing_response (username, prompt_id, writing_response) VALUES ($1, $2, $3)', [username, prompt_id, writing_response]);
+      res.status(201).send('Data inserted successfully');
+    } catch (err) {
+      console.error('Error executing query', err);
+      res.status(500).send('Error inserting data');
+    }
+  });
+
+
 const PORT = process.env.PORT || 8300;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

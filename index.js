c033c8b0b2a93b5391df42f9615268bb93eaa057
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2024-04-02 20:06:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5500"
+  origin: "*"
 }));
 
 const dbURI = process.env.POSTGRES_URL;

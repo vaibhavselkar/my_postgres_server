b9e5b21f1de88ba793ed46997a7bcfb83046c4a2
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2024-04-24 14:35:37 +0530

    Update index.js - prepo collection added

--- index.js
+++ index.js
@@ -62,6 +62,176 @@ client.connect()
     }
   });
 
+  app.get('/prepo-time', async (req, res) => {
+    try {
+      const query = 'select * from "preposition_of_time"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'prepositions' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'prepositions' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+  app.get('/prepo-place', async (req, res) => {
+    try {
+      const query = 'select * from "preposition_of_place"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'prepositions' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'prepositions' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+  app.get('/prepo-direction', async (req, res) => {
+    try {
+      const query = 'select * from "preposition_of_direction"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'prepositions' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'prepositions' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+  app.get('/prepo-agent', async (req, res) => {
+    try {
+      const query = 'select * from "preposition_of_agent"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'prepositions' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'prepositions' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+  app.get('/prepo-phrases', async (req, res) => {
+    try {
+      const query = 'select * from "prepositional_phrases"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'prepositions' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'prepositions' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+  app.get('/prepo-participle', async (req, res) => {
+    try {
+      const query = 'select * from "participle_prepositions"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'prepositions' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'prepositions' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+  app.get('/prepo-confusion', async (req, res) => {
+    try {
+      const query = 'select * from "confusing_prepositions"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'prepositions' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'prepositions' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+  app.get('/prepo-compound', async (req, res) => {
+    try {
+      const query = 'select * from "compound_prepositions"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'prepositions' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'prepositions' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+  app.get('/prepo-beside', async (req, res) => {
+    try {
+      const query = 'select * from "confusing_beside_and_besides"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'prepositions' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'prepositions' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+  app.get('/prepo-on/far', async (req, res) => {
+    try {
+      const query = 'select * from "confusing_on_and_for"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'prepositions' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'prepositions' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+  app.get('/prepo-between', async (req, res) => {
+    try {
+      const query = 'select * from "misplacing_between_and_among"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'prepositions' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'prepositions' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+  app.get('/prepo-of', async (req, res) => {
+    try {
+      const query = 'select * from "missuse_of_on_at_in"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'prepositions' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'prepositions' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+// Vocabulary
+  app.get('/vocab5', async (req, res) => {
+    try {
+      const query = 'select * from "Vocab_basics_1"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'RC' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'RC' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
+// Reading Comprehension
+  app.get('/rc1', async (req, res) => {
+    try {
+      const query = 'select * from "Rc_trial"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'RC' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'RC' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
+
 // Reading Comprehension
   app.get('/rc1', async (req, res) => {
     try {
@@ -101,6 +271,17 @@ client.connect()
     }
   });
 
+   app.get('/diagnos-math-0', async (req, res) => {
+    try {
+      const query = 'select * from "Maths_Diagnostic Test For Grades 1-4"';
+      const result = await client.query(query);
+      console.log("Successfully retrieved data from 'diagnostic-math-1' table:", result.rows);
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error retrieving data from 'diagnostic-math-1' table:", error);
+      res.status(500).json({ error: "An error occurred while retrieving data" });
+    }
+  });
 
    app.get('/diagnos-math-1', async (req, res) => {
     try {
